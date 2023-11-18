5622504606b6b4daae7cecb8abbe832d09e70047
lolka0d <fockork@gmail.com>
2023-11-18 20:16:03 +0200

    Created a minimal generation

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import glob
+from fpdf import FPDF
+from pathlib import Path
+
+filepaths = glob.glob("invoices/*.xlsx")
+
+for filepath in filepaths:
+    filename = Path(filepath).stem
+    number = filename.split('-')[0]
+    date = filename.split('-')[1]
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt="Invoice nr.{}".format(number))
+    pdf.output("PDFs/{}.pdf".format(filename))
